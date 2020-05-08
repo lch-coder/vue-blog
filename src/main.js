1b19aea9a5b8e969ca9f6252d131a4aadafca51c
lch <1660396419@qq.com>
2020-05-08 23:06:09 +0800

    图片上传、删除

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ Vue.use(ElementUI)
 Vue.use(mavonEditor)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   router,
   render: h => h(App)
--- src/services/utils/axios.js
+++ src/services/utils/axios.js
@@ -2,15 +2,19 @@ import axios from 'axios'
 import {baseUrl} from '../../config/axiosConfig'
 import ElementUI from 'element-ui'
 
-axios.defaults.timeout = 60000
-axios.defaults.withCredentials = true
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
-axios.defaults.headers['Cache-Control'] = 'no-cache,no-store,must-revalidate,max-age=-1,private'
-axios.defaults.baseURL = baseUrl
+const service = axios.create({
+  baseURL: baseUrl
+})
+
+service.defaults.timeout = 60000
+service.defaults.withCredentials = true
+service.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+service.defaults.headers['Access-Control-Allow-Origin'] = '*'
+service.defaults.headers['Cache-Control'] = 'no-cache,no-store,must-revalidate,max-age=-1,private'
+// service.defaults.baseURL = baseUrl
 
 // 请求发送前拦截
-axios.interceptors.request.use(config => {
+service.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
   config.headers.common['Authorization'] = 'Bearer ' + token
   return config
@@ -18,8 +22,9 @@ axios.interceptors.request.use(config => {
   console.log(error)
   Promise.reject(error)
 })
-// http响应拦截器
-axios.interceptors.response.use(res => {
+
+// 响应拦截器
+service.interceptors.response.use(res => {
   return res
 },
 error => {
@@ -27,10 +32,7 @@ error => {
   if (error.response.status) {
     switch (error.response.status) {
       case 400:
-        msg = '请求错误(400)，请重新申请'
-        break
-      case 401:
-        msg = '登录错误(401)，请重新登录'
+        msg = '请求错误，请稍后重试'
         break
       case 403:
         msg = '拒绝访问(403)'
@@ -38,26 +40,8 @@ error => {
       case 404:
         msg = '请求出错(404)'
         break
-      case 408:
-        msg = '请求超时(408)'
-        break
       case 500:
         msg = '服务器错误(500)，请稍后重试！'
-        break
-      case 501:
-        msg = '服务未实现(501)'
-        break
-      case 502:
-        msg = '网络错误(502)'
-        break
-      case 503:
-        msg = '服务不可用(503)'
-        break
-      case 504:
-        msg = '网络超时(504)'
-        break
-      case 505:
-        msg = 'HTTP版本不受支持(505)'
         break
       default:
         msg = '网络连接出错'
@@ -74,14 +58,14 @@ error => {
 
 export default {
   setAxiosGetPromise: (url, params = {}) => {
-    return axios.get(timestamp(url), {params: params}).then(response => {
+    return service.get(timestamp(url), {params: params}).then(response => {
       return response
     }).catch(err => {
       throw err
     })
   },
   setAxiosPostPromise: (url, data) => {
-    return axios.post(url, data).then(response => {
+    return service.post(url, data).then(response => {
       return response
     }).catch(err => {
       throw err
@@ -89,14 +73,14 @@ export default {
   },
   // 更新全部
   setAxiosPutPromise: (url, data) => {
-    return axios.put(url, data).then(response => {
+    return service.put(url, data).then(response => {
       return response
     }).catch(err => {
       throw err
     })
   },
   setAxiosDeletePromise: (url, data) => {
-    return axios.delete(url, {data: data}).then(response => {
+    return service.delete(url, {data: data}).then(response => {
       return response
     }).catch(err => {
       throw err
